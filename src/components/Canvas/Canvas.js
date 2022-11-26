b1d73b58579258fae7f87b1d22aad9e51e65176c
K1ttK1tty <vlad.petuxov.2018@bk.ru>
2022-11-26 17:59:31 +0700

    coordinate system done, sin done

---
+++ src/components/Canvas/Canvas.js
@@ -0,0 +1,23 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Canvas = ({ draw, height, width }) => {
+  const canvas = React.useRef();
+
+  React.useEffect(() => {
+    const context = canvas.current.getContext("2d");
+    const canvasWidth = width;
+    const canvasHeight = height;
+    draw(context, canvasWidth, canvasHeight);
+  });
+
+  return <canvas ref={canvas} height={height} width={width} />;
+};
+
+Canvas.propTypes = {
+  draw: PropTypes.func.isRequired,
+  height: PropTypes.number.isRequired,
+  width: PropTypes.number.isRequired,
+};
+
+export default Canvas;
